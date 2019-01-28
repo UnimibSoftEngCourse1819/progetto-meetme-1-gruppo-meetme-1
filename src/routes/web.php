36bfb8223a23f735213ee3a66f6ca3b34ffb7e75
Davide Marchetti <dvdmarchetti663@gmail.com>
2019-01-28 14:43:20 +0100

    Added basic routing and removed unnecessary default routes.

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'WelcomeController');
+
+Route::resource('events', 'EventsController');
+
+Route::prefix('/survey')->name('surveys.')->group(function () {
+    Route::get('/{survey}', 'SurveysController@show')->name('show');
+    Route::post('/{survey}', 'SurveysController@answer')->name('answer');
+});
+
+Route::prefix('/account')->name('account.')->group(function () {
+    Route::get('/settings', 'SettingsController@index')->name('settings');
+    Route::patch('/settings', 'SettingsController@update')->name('settings.update');
+
+    Route::get('/users', 'UsersController@index')->name('users');
+    Route::patch('/users', 'UsersController@update')->name('users.update');
+
+    Route::get('/calendars', 'CalendarsController@index')->name('calendars');
+    Route::post('/calendars/sync', 'CalendarsController@sync')->name('calendars.sync');
 });
