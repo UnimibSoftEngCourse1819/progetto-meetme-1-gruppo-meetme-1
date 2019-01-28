e6053ceb1d0f29d83776c15c96242692e7155cff
Paolo Bacchiega <p.bacchiega1@campus.unimib.it>
2019-01-28 18:32:39 +0100

    Added basic seeders

--- src/database/seeds/CompaniesTableSeeder.php
+++ src/database/seeds/CompaniesTableSeeder.php
@@ -1,16 +1,21 @@
 <?php
 
+use App\User;
+use App\Company;
+
 use Illuminate\Database\Seeder;
 
 class CompaniesTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Generate 10 companies associated to 10 random users
      *
      * @return void
      */
     public function run()
     {
-        //
+        User::all()->random(10)->each(function ($user) {
+            $user->company()->save(factory(Company::class)->make());
+        });
     }
 }
--- src/database/seeds/EmailsTableSeeder.php
+++ src/database/seeds/EmailsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use App\Company;
 use App\Email;
 
 use Illuminate\Database\Seeder;
@@ -8,14 +9,21 @@ use Illuminate\Database\Seeder;
 class EmailsTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Generate emails' table seed
      *
      * @return void
      */
     public function run()
     {
-        User::all()->each(function ($user) {
-            $user->emails()->save(factory(Email::class)->make());
+        $users = User::all();
+        $users->each(function ($user) {
+            $user->emails()->saveMany(factory(Email::class, 3)->make());
+        });
+
+        $companies = Company::all();
+        $companies->each(function ($company) use ($users){
+            $emails = $users-> random(3)->pluck('emails.0');
+            $company->emails()->saveMany($emails);
         });
     }
 }
