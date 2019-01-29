43ab1898fd400d51b4eac7cbc304a272ae0f052f
Paolo Bacchiega <p.bacchiega1@campus.unimib.it>
2019-01-29 16:21:13 +0100

    Created Logout form

--- src/resources/views/layouts/partials/header.blade.php
+++ src/resources/views/layouts/partials/header.blade.php
@@ -1,26 +1,33 @@
-<div class="ui borderless secondary pointing small menu">
-    <div class="item"><img src="/images/logo.png"></div>
+<div class="ui secondary pointing small menu">
+    <div class="item">
+        <img src="/images/logo.png">
+        </div>
     <a class="active item" href="{{ url('/') }}">MeetMe</a>
-    <div class="right menu">
+        <div class="right menu">
         <div class="stretched collapsed row">
             @auth
-                <a class="item" href="{{ url('/home') }}">Dashboard</a>
-                <a class="item" href="{{ route('events.index') }}">Eventi</a>
+                <div class="right menu">
+                    <a class="item" href="{{ url('/home') }}">Dashboard</a>
+                    <a class="item" href="{{ route('events.index') }}">Eventi</a>
+                    <a class="item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                       document.getElementById('logout-form').submit();"> Logout </a>
 
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </div>
             @else
                 @if (Route::has('register'))
                     <div class="item">
-
                         <a href="{{ route('register') }}" class="ui compact primary button">Sign up</a>
-
                     </div>
                 @endif
-
-                <di class="item">
+                <div class="item">
                     <a href="{{ route('login') }}" class="ui compact button">Log-in</a>
 
                 </div>
             @endauth
         </div>
-    </div>
+        </div>
+
 </div>
