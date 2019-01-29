4354a424178e60edf92a93c08d5ba2bc31d0c46d
Matteo Messenzani <matteosmart97@gmail.com>
2019-01-29 17:07:15 +0100

    Added Semantic UI Email and Reset

--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="ui padded text container segment">
+        @if (session('status'))
+            <div class="ui message success" role="alert">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
+
+        @if ($errors->any())
+            <div class="ui message error" role="alert">
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </div>
+        @endif
+
+        <form class="ui form" method="POST" action="{{ route('password.email') }}">
+            @csrf
+            <div class="field {{ $errors->has('email') ? ' error' : '' }}">
+                <label>Email</label>
+                <input type="email" name="email" placeholder="email" value="{{ $email ?? old('email') }}">
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
+            </div>
+            <button class="ui button" type="submit">{{ __('Send Password Reset Link') }}</button>
+        </form>
     </div>
-</div>
 @endsection
