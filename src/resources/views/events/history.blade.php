d7e34c9137611c6885b241e28fbea6cf4def5a1f
Matteo Messenzani <matteosmart97@gmail.com>
2019-01-29 19:00:49 +0100

    Added Semantic UI to History

--- src/resources/views/events/history.blade.php
+++ src/resources/views/events/history.blade.php
@@ -1,26 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('My Events') }}</div>
+<div class="ui text container">
+    <div class="ui header">{{ __('My Events') }}</div>
 
-                <div class="card-body">
-                    @foreach (request()->user()->emails as $email)
-                        <p>Email: {{ $email->email }}</p>
-                        <ul>
-                            @forelse ($email->events as $event)
-                                <li><a href="{{ route('events.show', ['id' => $event->id]) }}">{{ $event->title }} @if (! $event->public)(Private)@endif</a></li>
-                            @empty
-                                <p>No events.</p>
-                            @endforelse
-                        </ul>
-                    @endforeach
-                </div>
-            </div>
+    @foreach (request()->user()->emails as $email)
+        <div class="ui segment">
+            <div class="ui small header"> <i class="big envelope icon"> </i> </span> {{ $email->email }}</div>
+            <ul>
+                @forelse ($email->events as $event)
+                    <li>
+                        <div class ="ui list">
+                            <div class="item">
+                                <div class="header">
+                                     <a href="{{ route('events.show', ['id' => $event->id]) }}">
+                                         {{ $event->title }}
+                                         @if ($event->public)
+                                             <span class="ui mini blue label">Public</span>
+                                         @else
+                                             <span class="ui mini orange label">Private</span>
+                                         @endif
+
+                                      </a>
+                                </div>
+                            {{ $event->created_at->diffForHumans() }}
+                            </div>
+                        </div>
+                    </li>
+                @empty
+                    <p>No events.</p>
+                @endforelse
+            </ul>
         </div>
-    </div>
+    @endforeach
 </div>
 @endsection
