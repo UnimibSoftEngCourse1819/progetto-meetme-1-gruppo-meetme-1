be63d6c945e6b02692ff26508519a49dc9c4fb62
Filippo Massi <f.massi2@campus.unimib.it>
2019-01-31 00:19:07 +0100

    home page edit

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,4 +1,42 @@
 @extends('layouts.app')
+
+
+@section('custom-styles')
+
+<style>
+	.pushable .pusher {
+		padding-top: 3rem;
+	}
+	.content {
+		position: relative;
+		z-index: 99;
+		padding-top: 5rem;
+	}
+	.bg {
+		background-image : url({{asset('/img/background.jpg')}});
+		background-size: cover;
+		overflow: hidden;
+		position: fixed;
+		width: 100%;
+		height: 100%;
+	}
+	.bg:after {
+		content: '';
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+
+		background-color: rgba(0, 0, 0, .4);
+	}
+</style>
+
+@endsection
+
 @section('content')
-    welcome
+<div class="bg"></div>
+<div class="content">
+	<h1>asdgfjsdf</h1>
+</div>
 @endsection
