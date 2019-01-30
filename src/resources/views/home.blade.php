93894896fcdc8ce84ee069d0a185d607a1de2f99
Paolo Bacchiega <p.bacchiega1@campus.unimib.it>
2019-01-30 15:54:04 +0100

    Added Dashboard content. Fixed general layout

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -10,11 +10,9 @@
                     <i class="circular calendar check outline icon"></i>
                     {{$part_events}} eventi in programma
                 </h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
+
             </div>
+
             <div class="column">
                 <h2 class="ui center aligned icon header">
                     <i class="circular calendar plus outline icon"></i>
@@ -26,6 +24,24 @@
         O
         </div>
     </div>
+    <div class="ui internally celled grid">
+        <div class="row">
+            <div class="eight wide column">
+                <div class="row">
+                    @foreach ($events as $event)
+                        <li>{{ $event->title }} {{ (new \Carbon\Carbon($event->created_at))->diffForHumans() }}</li>
+                    @endforeach
+                </div>
+            </div>
+            <div class="eight wide column">
+                <div class="row">
+                    @foreach ($owned_event as $owned)
+                        <li>{{ $owned->title }} {{ (new \Carbon\Carbon($owned->created_at))->diffForHumans() }}</li>
+                    @endforeach
+                </div>
+            </div>
 
+        </div>
+    </div>
 </div>
 @endsection
