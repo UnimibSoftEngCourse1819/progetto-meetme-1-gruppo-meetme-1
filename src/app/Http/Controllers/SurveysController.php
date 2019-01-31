008c748568d469ec337c1ed5b950e70cc8f7cd14
Filippo Massi <f.massi2@campus.unimib.it>
2019-01-31 16:21:30 +0100

    surveys controller

--- src/app/Http/Controllers/SurveysController.php
+++ src/app/Http/Controllers/SurveysController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
+use App\TimeSlot;
 use Illuminate\Http\Request;
 
 class SurveysController extends Controller
@@ -14,7 +16,10 @@ class SurveysController extends Controller
      */
     public function show(Event $survey)
     {
-        //
+        //dd($survey->id);
+        $time_slots = $survey->timeslots;
+        //$voters = $time_slots->voters;
+        return view('surveys.show', compact('time_slots', "survey"));
     }
 
     /**
