25d2f00f2e4778b2e388736cbbea882c413022be
Davide Marchetti <dvdmarchetti663@gmail.com>
2019-01-31 16:59:48 +0100

    Secured account routes

--- src/app/Http/Controllers/EmailsController.php
+++ src/app/Http/Controllers/EmailsController.php
@@ -8,6 +8,15 @@ use Illuminate\Http\Request;
 
 class EmailsController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     /**
      * Store a newly created resource in storage.
