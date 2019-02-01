c248782bb1022b4d69bcddb2e3745478350d4970
Paolo Bacchiega <p.bacchiega1@campus.unimib.it>
2019-02-01 11:30:17 +0100

    Fixed parallax motive. Added js libraries

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Scripts -->
     <!--<script src="{{ asset('js/app.js') }}" defer></script>-->
 
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
@@ -19,6 +20,8 @@
     <!---Semantic ui-->
     @yield('custom-styles')
     <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css" rel="stylesheet">
+
     <style>
         #content {
             min-height: 100px;
@@ -59,7 +62,7 @@
                 display:none;
             }
             #mobile-content{
-                padding-top:0rem;
+                padding-top: -3rem;
             }
         }
 
