6dbf8ceff37f9e7aab5fa5ad035846896b6b085a
Matteo Messenzani <matteosmart97@gmail.com>
2019-02-05 18:27:15 +0100

    Better looking / Fixed History

--- src/resources/views/events/history.blade.php
+++ src/resources/views/events/history.blade.php
@@ -9,33 +9,43 @@
             </a>
     </div>
 
+    <div class="ui top attached tabular menu" id="emails">
+        @foreach (request()->user()->emails as $email)
+            <a class="@if ($loop->index == 0) active @endif item" data-tab="tab-{{ $loop->index }}">
+                {{ $email->email }}
+            </a>
+        @endforeach
+    </div>
     @foreach (request()->user()->emails as $email)
-        <div class="ui segment">
-            <div class="ui small header"> <i class="big envelope icon"> </i>  {{ $email->email }}</div>
-            <div class ="ui celled very relaxed list">
-                @forelse ($email->events as $event)
-                    <div class="item">
-                        <div class="header">
-                            <i class="calendar alternate outline icon"></i>
+        <div class="ui bottom attached segment @if ($loop->index == 0) active @endif tab" data-tab="tab-{{ $loop->index }}">
+            @forelse ($email->events as $event)
+                <div class="item">
+                    <div class="header">
+                        <i class="calendar alternate outline icon"></i>
 
-                            <a class="floated content" href="{{ route('events.show', ['id' => $event->id]) }}">
-                                <h5 class="ui header">
-                                    {{ $event->title }}
-                                    @if ($event->public)
-                                        <span class="circular ui mini blue label">Public</span>
-                                    @else
-                                        <span class="circular ui mini orange label">Private</span>
-                                    @endif
-                                </h5>
-                            </a>
-                        </div>
-                        {{ $event->created_at->diffForHumans() }}
+                        <a class="floated content" href="{{ route('events.show', ['id' => $event->id]) }}">
+                            <h5 class="ui header">
+                                {{ $event->title }}
+                                @if ($event->public)
+                                    <span class="circular ui mini blue label">Public</span>
+                                @else
+                                    <span class="circular ui mini orange label">Private</span>
+                                @endif</h5>
+                        </a>
                     </div>
-                @empty
-                    <p>No events.</p>
-                @endforelse
-            </div>
+                    {{ $event->created_at->diffForHumans() }}
+                </div>
+            @empty
+                <p>No events.</p>
+            @endforelse
         </div>
     @endforeach
-</div>
+@endsection
+
+@section('custom-scripts')
+<script>
+$(document).ready(function () {
+    $('#emails.menu .item').tab();
+});
+</script>
 @endsection
